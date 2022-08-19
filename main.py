9fde4e66ba80886739016576d2f55547f874744c
CaniTanushaj <hasan.tanushaj@fsre.sum.ba>
2022-08-19 17:42:34 +0200

    popravak

--- main.py
+++ main.py
@@ -1,154 +1,154 @@
-# MODULES
+# moduli
 import pygame, sys
 import numpy as np
 
-# initializes pygame
+# inicijalizacija igre
 pygame.init()
 
-# ---------
-# CONSTANTS
-# ---------
-WIDTH = 600
-HEIGHT = 600
-LINE_WIDTH = 15
-WIN_LINE_WIDTH = 15
-BOARD_ROWS = 3
-BOARD_COLS = 3
-SQUARE_SIZE = 200
-CIRCLE_RADIUS = 60
-CIRCLE_WIDTH = 15
-CROSS_WIDTH = 25
-SPACE = 55
-# rgb: red green blue
-RED = (255, 0, 0)
-BG_COLOR = (28, 170, 156)
-LINE_COLOR = (23, 145, 135)
-CIRCLE_COLOR = (239, 231, 200)
-CROSS_COLOR = (66, 66, 66)
 
+
+# boje
+
+pozadina = (255,255,255)
+BOJA_LINIJE = (0, 0, 0)
+BOJA_KRUGA = (0,0,0)
+BOJA_IKSA = (0,0,0)
+
+
+# konstante
+
+SIRINA = 600
+VISINA = 600
+DEBLJINA_LINIJE = 15
+CRTA = 15
+VELICINA_POLJA = 200
+PROMJER_KRUGA = 60
+SIRINA_KRUGA = 15
+SIRINA_IKSA = 25
+RAZMAK = 55
 # ------
-# SCREEN
+# graficki prikaz
 # ------
-screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
-pygame.display.set_caption( 'TIC TAC TOE' )
-screen.fill( BG_COLOR )
+displej = pygame.display.set_mode( (SIRINA, VISINA) )
+pygame.display.set_caption( 'Iks Oks' )
+displej.fill( pozadina )
 
 # -------------
-# CONSOLE BOARD
+# logicka ploca
 # -------------
-board = np.zeros( (BOARD_ROWS, BOARD_COLS) )
+ploca = np.zeros( (3, 3) )
 
 # ---------
-# FUNCTIONS
+# funkcije
 # ---------
-def draw_lines():
+def crtaj_linije():
 	# 1 horizontal
-	pygame.draw.line( screen, LINE_COLOR, (0, SQUARE_SIZE), (WIDTH, SQUARE_SIZE), LINE_WIDTH )
+	pygame.draw.line( displej, BOJA_LINIJE, (30, VELICINA_POLJA), (SIRINA-30, VELICINA_POLJA),CRTA)
 	# 2 horizontal
-	pygame.draw.line( screen, LINE_COLOR, (0, 2 * SQUARE_SIZE), (WIDTH, 2 * SQUARE_SIZE), LINE_WIDTH )
+	pygame.draw.line( displej, BOJA_LINIJE, (30, 2 * VELICINA_POLJA), (SIRINA-30, 2 * VELICINA_POLJA),CRTA)
 
 	# 1 vertical
-	pygame.draw.line( screen, LINE_COLOR, (SQUARE_SIZE, 0), (SQUARE_SIZE, HEIGHT), LINE_WIDTH )
+	pygame.draw.line( displej, BOJA_LINIJE, (VELICINA_POLJA, 30), (VELICINA_POLJA, VISINA-30),CRTA)
 	# 2 vertical
-	pygame.draw.line( screen, LINE_COLOR, (2 * SQUARE_SIZE, 0), (2 * SQUARE_SIZE, HEIGHT), LINE_WIDTH )
+	pygame.draw.line( displej, BOJA_LINIJE, (2 * VELICINA_POLJA, 30), (2 * VELICINA_POLJA, VISINA-30),DEBLJINA_LINIJE )
 
-def draw_figures():
-	for row in range(BOARD_ROWS):
-		for col in range(BOARD_COLS):
-			if board[row][col] == 1:
-				pygame.draw.circle( screen, CIRCLE_COLOR, (int( col * SQUARE_SIZE + SQUARE_SIZE//2 ), int( row * SQUARE_SIZE + SQUARE_SIZE//2 )), CIRCLE_RADIUS, CIRCLE_WIDTH )
-			elif board[row][col] == 2:
-				pygame.draw.line( screen, CROSS_COLOR, (col * SQUARE_SIZE + SPACE, row * SQUARE_SIZE + SQUARE_SIZE - SPACE), (col * SQUARE_SIZE + SQUARE_SIZE - SPACE, row * SQUARE_SIZE + SPACE), CROSS_WIDTH )
-				pygame.draw.line( screen, CROSS_COLOR, (col * SQUARE_SIZE + SPACE, row * SQUARE_SIZE + SPACE), (col * SQUARE_SIZE + SQUARE_SIZE - SPACE, row * SQUARE_SIZE + SQUARE_SIZE - SPACE), CROSS_WIDTH )
+def crtaj_figure():
+	for row in range(3):
+		for col in range(3):
+			if ploca[row][col] == 1:
+				pygame.draw.circle( displej, BOJA_KRUGA, (int( col * VELICINA_POLJA + VELICINA_POLJA//2 ), int( row * VELICINA_POLJA + VELICINA_POLJA//2 )), PROMJER_KRUGA, SIRINA_KRUGA )
+			elif ploca[row][col] == 2:
+				pygame.draw.line( displej, BOJA_IKSA, (col * VELICINA_POLJA + RAZMAK, row * VELICINA_POLJA + VELICINA_POLJA - RAZMAK), (col * VELICINA_POLJA + VELICINA_POLJA - RAZMAK, row * VELICINA_POLJA + RAZMAK), SIRINA_IKSA)
+				pygame.draw.line( displej, BOJA_IKSA, (col * VELICINA_POLJA + RAZMAK, row * VELICINA_POLJA + RAZMAK), (col * VELICINA_POLJA + VELICINA_POLJA - RAZMAK, row * VELICINA_POLJA + VELICINA_POLJA - RAZMAK), SIRINA_IKSA )
 
-def mark_square(row, col, player):
-	board[row][col] = player
+def oznaci_polje(row, col, igrac):
+	ploca[row][col] = igrac
 
-def available_square(row, col):
-	return board[row][col] == 0
+def dostupno_polje(row, col):
+	return ploca[row][col] == 0
 
-def is_board_full():
-	for row in range(BOARD_ROWS):
-		for col in range(BOARD_COLS):
-			if board[row][col] == 0:
+def puna_ploca():
+	for row in range(3):
+		for col in range(3):
+			if ploca[row][col] == 0:
 				return False
 
 	return True
 
-def check_win(player):
+def pobjeda(igrac):
 	# vertical win check
-	for col in range(BOARD_COLS):
-		if board[0][col] == player and board[1][col] == player and board[2][col] == player:
-			draw_vertical_winning_line(col, player)
+	for col in range(3):
+		if ploca[0][col] == igrac and ploca[1][col] == igrac and ploca[2][col] == igrac:
+			crtaj_vertikalnu_dobitnu_liniju(col, igrac)
 			return True
 
 	# horizontal win check
-	for row in range(BOARD_ROWS):
-		if board[row][0] == player and board[row][1] == player and board[row][2] == player:
-			draw_horizontal_winning_line(row, player)
+	for row in range(3):
+		if ploca[row][0] == igrac and ploca[row][1] == igrac and ploca[row][2] == igrac:
+			crtaj_horizontalnu_dobitnu_liniju(row, igrac)
 			return True
 
 	# asc diagonal win check
-	if board[2][0] == player and board[1][1] == player and board[0][2] == player:
-		draw_asc_diagonal(player)
+	if ploca[2][0] == igrac and ploca[1][1] == igrac and ploca[0][2] == igrac:
+		crtaj_kosu_dobitnu_liniju(igrac)
 		return True
 
 	# desc diagonal win chek
-	if board[0][0] == player and board[1][1] == player and board[2][2] == player:
-		draw_desc_diagonal(player)
+	if ploca[0][0] == igrac and ploca[1][1] == igrac and ploca[2][2] == igrac:
+		crtaj_obrnutu_kosu_dobitnu_liniju(igrac)
 		return True
 
 	return False
 
-def draw_vertical_winning_line(col, player):
-	posX = col * SQUARE_SIZE + SQUARE_SIZE//2
+def crtaj_vertikalnu_dobitnu_liniju(col, igrac):
+	posX = col * VELICINA_POLJA + VELICINA_POLJA//2
 
-	if player == 1:
-		color = CIRCLE_COLOR
-	elif player == 2:
-		color = CROSS_COLOR
+	if igrac == 1:
+		boja = BOJA_KRUGA
+	elif igrac == 2:
+		boja = BOJA_IKSA
 
-	pygame.draw.line( screen, color, (posX, 15), (posX, HEIGHT - 15), LINE_WIDTH )
+	pygame.draw.line( displej, boja, (posX, 15), (posX, VISINA - 15),DEBLJINA_LINIJE )
 
-def draw_horizontal_winning_line(row, player):
-	posY = row * SQUARE_SIZE + SQUARE_SIZE//2
+def crtaj_horizontalnu_dobitnu_liniju(row, igrac):
+	posY = row * VELICINA_POLJA + VELICINA_POLJA//2
 
-	if player == 1:
-		color = CIRCLE_COLOR
-	elif player == 2:
-		color = CROSS_COLOR
+	if igrac == 1:
+		boja = BOJA_KRUGA
+	elif igrac == 2:
+		boja = BOJA_IKSA
 
-	pygame.draw.line( screen, color, (15, posY), (WIDTH - 15, posY), WIN_LINE_WIDTH )
+	pygame.draw.line( displej, boja, (15, posY), (SIRINA - 15, posY), CRTA )
 
-def draw_asc_diagonal(player):
-	if player == 1:
-		color = CIRCLE_COLOR
-	elif player == 2:
-		color = CROSS_COLOR
+def crtaj_kosu_dobitnu_liniju(igrac):
+	if igrac == 1:
+		boja = BOJA_KRUGA
+	elif igrac == 2:
+		boja = BOJA_IKSA
 
-	pygame.draw.line( screen, color, (15, HEIGHT - 15), (WIDTH - 15, 15), WIN_LINE_WIDTH )
+	pygame.draw.line( displej, boja, (15, VISINA - 15), (SIRINA - 15, 15), CRTA )
 
-def draw_desc_diagonal(player):
-	if player == 1:
-		color = CIRCLE_COLOR
-	elif player == 2:
-		color = CROSS_COLOR
+def crtaj_obrnutu_kosu_dobitnu_liniju(igrac):
+	if igrac == 1:
+		boja = BOJA_KRUGA
+	elif igrac == 2:
+		boja = BOJA_IKSA
 
-	pygame.draw.line( screen, color, (15, 15), (WIDTH - 15, HEIGHT - 15), WIN_LINE_WIDTH )
+	pygame.draw.line( displej, boja, (15, 15), (SIRINA - 15, VISINA - 15), CRTA )
 
 def restart():
-	screen.fill( BG_COLOR )
-	draw_lines()
-	for row in range(BOARD_ROWS):
-		for col in range(BOARD_COLS):
-			board[row][col] = 0
+	displej.fill( pozadina )
+	crtaj_linije()
+	for row in range(3):
+		for col in range(3):
+			ploca[row][col] = 0
 
-draw_lines()
+crtaj_linije()
 
 # ---------
 # VARIABLES
 # ---------
-player = 1
+igrac = 1
 game_over = False
 
 # --------
@@ -164,22 +164,22 @@ while True:
 			mouseX = event.pos[0] # x
 			mouseY = event.pos[1] # y
 
-			clicked_row = int(mouseY // SQUARE_SIZE)
-			clicked_col = int(mouseX // SQUARE_SIZE)
+			clicked_row = int(mouseY // VELICINA_POLJA)
+			clicked_col = int(mouseX // VELICINA_POLJA)
 
-			if available_square( clicked_row, clicked_col ):
+			if dostupno_polje( clicked_row, clicked_col ):
 
-				mark_square( clicked_row, clicked_col, player )
-				if check_win( player ):
+				oznaci_polje( clicked_row, clicked_col, igrac )
+				if pobjeda( igrac ):
 					game_over = True
-				player = player % 2 + 1
+				igrac = igrac % 2 + 1
 
-				draw_figures()
+				crtaj_figure()
 
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_r:
 				restart()
-				player = 1
+				igrac = 1
 				game_over = False
 
 	pygame.display.update()
